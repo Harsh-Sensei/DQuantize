c047392131871016e59bbd48134c9e5583f259fe
nidhihegde001 <nidhihegde001@gmail.com>
2025-11-28 16:29:33 -0500

    add slurm script for llada inference

---
+++ scripts/launch_llada_inference.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=gsm8kval_vanilla_llada_128_steps      # Job name
+#SBATCH --nodes=1                      # Number of nodes
+#SBATCH --gres=gpu:1                    # GPUs per node (change if node has >1 GPU)
+#SBATCH --cpus-per-task=8                # CPU cores per task
+#SBATCH --mem=64G                        # Memory per node
+#SBATCH --time=48:00:00                  # Walltime (hh:mm:ss)
+#SBATCH --partition=general              # Partition/queue name
+#SBATCH --output=logs/%x_%j.out   # Stdout log
+#SBATCH --error=logs/%x_%j.err    # Stderr log
+
+# Activate your conda environment
+# source /home/sreyasv/miniconda3/etc/profile.d/conda.sh
+# conda activate seer
+mkdir -p output
+module load cuda-12.5
+nvidia-smi
+
+source /home/nidhih/miniconda3/etc/profile.d/conda.sh
+conda activate colab
+
+cd /home/nidhih/isgrl/dquantize/DQuantize/dquantize
+
+python run_llada.py --output_dir='./outputs' --max_length=256 --dataset=gsm8k --max_examples=1319 --batch_size=64
